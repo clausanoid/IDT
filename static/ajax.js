b03baea3ca27a4c7b609c475b0fd47fbb656fc89
clausanoid <josesanchez150893@gmail.com>
2019-04-26 14:35:07 -0500

    Se agrego configuracion para grafica
    Verificar que el codigo funcione correctamente

---
+++ static/ajax.js
@@ -0,0 +1,40 @@
+$( document ).ready(function() {
+/* creamos una grafica de linea con informacion vacia
+            var dps = [];
+        /* var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "Distancia"
+	},
+	axisY:{
+		includeZero: false
+	},
+	data: [{
+		type: "line",
+		dataPoints: dps
+	}]
+});
+chart.render();
+
+*/
+
+
+    setTimeout(function()
+{
+    $.ajax({
+        type: "POST",
+        url: "http://localhost/sensor",
+        success: function(respuesta){
+          $('.center-div').html(respuesta); // vamos a escribir en el centro el valor de nuestra lectura
+          // Agregamos puntos a nuestra tabla a partir de los valores que recibimos del arduino
+          /* dps.push({
+			y: respuesta
+		}) */
+
+
+    });
+}, 10000); // Haremos una solicitud cada 10 segundos
+
+
+});
